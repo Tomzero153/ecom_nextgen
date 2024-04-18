c588c3db2ecf229e179d226489508c33728eab84
Tomzero153 <142730933+Tomzero153@users.noreply.github.com>
2024-04-18 15:34:51 +0700

    add email and check

--- app/user/signup/page.js
+++ app/user/signup/page.js
@@ -5,13 +5,16 @@
 import React, { useState, useEffect } from "react";
 import Link from "next/link";
 import Swal from "sweetalert2";
-import { useRouter } from 'next/navigation'
+import { useRouter } from "next/navigation";
 
 export default function Signup() {
   const [username, setUserName] = useState("");
+  const [email, setEmail] = useState("");
+  const [error_email, setError_email] = useState("");
   const [password, setPassword] = useState("");
+  const [error_password, setError_password] = useState("");
   const [confirmPassword, setConfirmPassword] = useState("");
-  const router = useRouter()
+  const router = useRouter();
 
   console.log(username, password, confirmPassword);
   const handleSubmit = async (e) => {
@@ -61,7 +64,7 @@ export default function Signup() {
         const form = e.target;
         Swal.fire("OK", "User registration successfully!", "success");
         form.reset();
-        router.push('/user/login');
+        router.push("/user/address");
       } else {
         Swal.fire("Oops...", "User registration failed.", "error");
         console.log("User registration failed.");
@@ -71,13 +74,70 @@ export default function Signup() {
     }
   };
 
+  const validateEmail = (email) => {
+    const re = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    return re.test(String(email).toLowerCase());
+  };
+  const email_onChange = (e) => {
+    const emailValue = e.target.value;
+    setEmail(emailValue);
+
+    if (validateEmail(emailValue)) {
+      setError_email("");
+      console.log("email", "true");
+    } else {
+      setError_email("Email worng formate");
+      // ใส่โค้ดสำหรับแสดงข้อความหรือการแจ้งเตือนถ้าอีเมลไม่ถูกต้อง
+    }
+  };
+
+
+  const confirmPassword_onChange = (e) => {
+    const passwordValue = e.target.value;
+    setConfirmPassword(passwordValue);
+
+    if (passwordValue == password  &&  passwordValue.length > 1) {
+      setError_password("");
+      console.log("email", "true");
+    } else {
+      setError_password("Password not match");
+      // ใส่โค้ดสำหรับแสดงข้อความหรือการแจ้งเตือนถ้าอีเมลไม่ถูกต้อง
+    }
+  };
+
   return (
     <div className="">
       <div className=" block xl:w-[1300px] w-full m-auto  items-center pt-20">
         <div className="sm:w-[600px] w-full m-auto items-center  bg-purple-950  rounded-md">
           <h1 className="pt-5 flex text-4xl justify-center">SIGN UP</h1>
           <form onSubmit={handleSubmit} className="pl-2  pt-8  w-full max-w-sm">
-            <div className="md:flex md:items-center mb-6">
+            <div className="md:flex md:items-center mb-8">
+              <div className="md:w-1/3">
+                <label
+                  className="block text-white  font-bold md:text-right mb-1 md:mb-0 pr-4"
+                  for="inline-full-name"
+                >
+                  Email
+                </label>
+              </div>
+              <div className="md:w-2/3 relative">
+                <input
+                  className="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-black leading-tight focus:outline-none  focus:border-purple-500"
+                  id="inline-full-name"
+                  type="text"
+                  placeholder="email"
+                  value={email}
+                  onChange={email_onChange}
+                />
+                {error_email && (
+                  <p className="text-red-500 absolute top-10 left-0">
+                    {error_email}
+                  </p>
+                )}
+              </div>
+            </div>
+
+            <div className="md:flex md:items-center mb-8">
               <div className="md:w-1/3">
                 <label
                   className="block text-white  font-bold md:text-right mb-1 md:mb-0 pr-4"
@@ -97,7 +157,7 @@ export default function Signup() {
                 />
               </div>
             </div>
-            <div className="md:flex md:items-center mb-6">
+            <div className="md:flex md:items-center mb-8">
               <div className="md:w-1/3">
                 <label
                   className="block text-white font-bold md:text-right mb-1 md:mb-0 pr-4"
@@ -117,7 +177,7 @@ export default function Signup() {
               </div>
             </div>
 
-            <div className="md:flex md:items-center mb-6">
+            <div className="md:flex md:items-center mb-8">
               <div className="md:w-1/3">
                 <label
                   className="block text-white font-bold md:text-right mb-1 md:mb-0 pr-4"
@@ -126,14 +186,19 @@ export default function Signup() {
                   Confirm Password
                 </label>
               </div>
-              <div className="md:w-2/3">
+              <div className="md:w-2/3 relative">
                 <input
                   className="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-black leading-tight focus:outline-none focus:bg-white focus:border-purple-500"
                   id="inline-password"
                   type="password"
                   placeholder="******************"
-                  onChange={(e) => setConfirmPassword(e.target.value)}
+                  onChange={confirmPassword_onChange}
                 />
+                     {error_password && (
+                  <p className="text-red-500 absolute top-10 left-0">
+                    {error_password}
+                  </p>
+                )}
               </div>
             </div>
             <div className="md:flex md:items-center mb-4">
